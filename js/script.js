9026fa848c788de1ce826a7e8e36b02ee1440848
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2023-01-03 20:37:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let currentScroll=0;
+let scrollAmt=1516;
+
+const sItems=document.querySelector('.section-2-items');
+const hScroll=document.querySelector('.horizontal-scroll');
+const btnScrollLeft=document.querySelector('#btn-scroll-left-1');
+const btnScrollRight=document.querySelector('#btn-scroll-right-1');
+
+
+let maxScroll=-sItems.offsetWidth +hScroll.offsetWidth+456;
+
+function scrollHorizontally(val){
+    currentScroll+=(val*scrollAmt);
+
+    if(currentScroll>0){
+        currentScroll=0;
+        btnScrollLeft.style.opacity="0";
+    }else{
+        btnScrollLeft.style.opacity="1";
+    }
+
+    if(currentScroll<maxScroll){
+        currentScroll=maxScroll;
+        btnScrollRight.style.opacity="0";
+    }else{
+        btnScrollRight.style.opacity="1";
+    }
+    
+    sItems.style.left=currentScroll+ "px";
+}
+
+btnScrollLeft.addEventListener("click",()=>{scrollHorizontally(1)});
+btnScrollRight.addEventListener("click",()=>{scrollHorizontally(-1)});
